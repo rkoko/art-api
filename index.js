4310e65566826aa4628f8616994b4771e33c0c50
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-07-07 00:10:50 -0400

    added some v basic styling with semantic UI and also replaced load-more button with infinite scroll

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(document).ready(function (){
   search()
+  nextPage()
 })
 
 
@@ -22,31 +23,36 @@ function dataReceived(data){
   console.log("hello")
   if (data.artObjects.length > 0) {
     data.artObjects.forEach((artwork) => {
+      debugger
       if (artwork.hasImage === true) {
-        $("#results").append(`<div>
-          <img src="${ artwork.webImage.url}" style="width:200px;height:200px"/>
+        $("#results").append(`
+          <div class="four wide column">
+          <img src="${ artwork.webImage.url}" style="max-width:200px;height:200px"/>
           <h3>${artwork.title}</h3>
           <p>${artwork.principalOrFirstMaker}</p>
           </div>`)
         }
     })
-    $("#loadMoreButtonDiv").append(`<button id="loadMore">Load More</button>`)
-    nextPage()
+    // $("#loadMoreButtonDiv").append(`<button id="loadMore" class="ui button">Load More</button>`)
+    // // nextPage()
   } else {
     $("#results").append("No Results")
   }
 }
 
 function nextPage() {
-  console.log("next")
-  $('#loadMore').on('click', function(event){
-    page += 1
-    $("#loadMore").remove()
-    getCollection(searchTerms, page)
-    event.preventDefault()
+  $(document).on('scroll', function(event){
+    console.log("scrolling!")
+    if ( window.innerHeight + window.scrollY > $(document).height() - 200 ) {
+        console.log("I need to load some more content here…");
+        page += 1
+            $("#loadMore").remove()
+            getCollection(searchTerms, page)
+            event.preventDefault()
+    }
   })
-
 }
+
 
 
 function displayError(){
